7b63cde41300231c9dec8693fff22ce89378dca4
Tales Henrique Bueno Rodrigues <talesbueno06@gmail.com>
2019-10-28 21:11:05 -0300

    Com processamento de arquivo de 4k em 4k

--- eclipse-workspace/CompressorRenan/src/br/com/renan/Model/Arquivo.java
+++ eclipse-workspace/CompressorRenan/src/br/com/renan/Model/Arquivo.java
@@ -1,13 +1,56 @@
 package br.com.renan.Model;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Arquivo {
 
 	public String lerArquivo(String caminho) {
+
+		String texto = "";
+		
+		try {
+
+			BufferedInputStream inputstream = new BufferedInputStream(
+				    new FileInputStream(caminho));
+
+			//4096 = 1024*4
+			byte[] dataAsByte = new byte[4096];
+			inputstream.read(dataAsByte, 0, 0);
+
+			int nRead;
+			
+			while ((nRead = inputstream.read(dataAsByte)) != -1) {
+
+				for (int i = 0; i < nRead; i++) {
+
+					//System.out.print((char) dataAsByte[i]);
+					texto = texto + (char) dataAsByte[i];
+
+				}
+			}
+			inputstream.close();
+			
+			return texto;
+
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	public String lerArquivoCompleto(String caminho) {
 
 		BufferedReader inputStream = null;
 		String texto = "", l;
